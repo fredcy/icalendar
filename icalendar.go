9ee1a70b90f942f1daf34e049b381a1c6746e7b2
Fred Yankowski <fcy@imsa.edu>
2013-11-22 16:54:37 -0600

    Define Value interface.

--- icalendar.go
+++ icalendar.go
@@ -2,54 +2,65 @@ package icalendar
 
 import (
 	"strings"
+	"time"
 )
 
 var crlf = "\r\n"
 
-type Value struct {
-	vString string
+type Name string
+func (n *Name) String() string { return strings.ToUpper(string(*n)) }
+
+type Value interface {
+	String() string
 }
 
+type VString string
+func (s VString) String() string { return string(s) }
+
+type VDate time.Time
+var datelayout = "20060102"
+func (d VDate) String() string { return time.Time(d).Format(datelayout) }
+
 type Parameter struct {
-	name string
-	value string
+	name Name
+	value Value
 }
 
 type Property struct {
-	name string
-	value string
+	name Name
+	value Value
 	parameters []Parameter
 }
 
 type Properties []Property
 
 type Component struct {
-	name string
+	name Name
 	properties Properties
 	components []Component
 }
 
 func (properties Properties) String() string {
-	return "dummy"
+	return "dummy TODO"
 }
 
 func (c *Component) String() string {
-	s := "BEGIN:" + c.name + crlf
+	s := "BEGIN:" + c.name.String() + crlf
 	for _, prop := range c.properties {
-		s += strings.ToUpper(prop.name) + ":" + prop.value + crlf
+		s += prop.name.String() + ":" + prop.value.String() + crlf
 	}
 	for _, subc := range c.components {
 		s += subc.String()
 	}
-	s += "END:" + c.name + crlf
+	s += "END:" + c.name.String() + crlf
 	return s
 }
 
-func (c *Component) Add(name, value string) {
+func (c *Component) Add(name Name, value Value) {
 	c.properties = append(c.properties, Property{name: name, value: value})
 }
 
-func (c *Component) Set(name, value string) {
+func (c *Component) Set(name Name, value Value) {
 	c.Add(name, value)			// TODO: remove duplicates
 }
 
--- icalendar_test.go
+++ icalendar_test.go
@@ -16,27 +16,27 @@ func Join(lines []string) string {
 }
 
 func TestVevent(t *testing.T) {
-	e := Component{ name: "VEVENT" }
-	e.Add("SUMMARY", "foo")
+	e := Component{ name: Name("VEVENT") }
+	e.Add("SUMMARY", VString("foo"))
 	MustEqual(t, e.String(),
 		Join([]string{"BEGIN:VEVENT", "SUMMARY:foo", "END:VEVENT"}))
 }
 
 func TestCalendar(t *testing.T) {
 	c := Component{ name: "VCALENDAR" }
-	c.Add("version", "2.0")
+	c.Add("version", VString("2.0"))
 	MustEqual(t, c.String(),
 		Join([]string{"BEGIN:VCALENDAR", "VERSION:2.0", "END:VCALENDAR"}))
 }
 
 func TestSubComponents(t *testing.T) {
 	timezone := Component{ name: "VTIMEZONE" }
-	timezone.Set("tzid", "America/Chicago")
+	timezone.Set("tzid", VString("America/Chicago"))
 	MustEqual(t, timezone.String(),
 		Join([]string{"BEGIN:VTIMEZONE", "TZID:America/Chicago", "END:VTIMEZONE"}))
 
 	daylight := Component{ name: "DAYLIGHT" }
-	daylight.Add("tzname", "CDT")
+	daylight.Add("tzname", VString("CDT"))
 	timezone.AddComponent(&daylight)
 	MustEqual(t, timezone.String(),
 		Join([]string{"BEGIN:VTIMEZONE", "TZID:America/Chicago", "BEGIN:DAYLIGHT",
